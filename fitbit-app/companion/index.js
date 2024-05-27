7d5c6f273e8ae815082fd93bc92357a446017693
afcomesana <albertofcomesana@gmail.com>
2024-05-27 15:04:21 +0200

    initial commit

---
+++ fitbit-app/companion/index.js
@@ -0,0 +1,139 @@
+import { inbox, outbox } from "file-transfer";
+
+/**
+ * Add functionality to the string type so that it can be parsed to an ArrayBuffer
+ * to be sent in file to the smartwatch.
+ * 
+ * @returns ArrayBuffer
+ */
+String.prototype.toArrayBuffer = function() {
+    // Buffer that will store the codes for each character, each of which
+    // takes two bytes (hence Uint16Array type)
+    const bufferedString = new Uint16Array(this.length);
+
+    for(let charIndex = 0; charIndex < this.length; charIndex++) {
+        bufferedString[charIndex] = this.charCodeAt(charIndex);
+    }
+    
+    return bufferedString;
+}
+
+let hostIp           = "",
+    hostPort         = 12345,
+    lookingForHostIp = false;
+
+// All the IP addresses that will be checked to find the host IP
+// This must be changed if the local IP address of the phone does
+// not start with 192.168.0 or 192.168.1
+const LOCAL_NETWORK_IP_ADDRESSES = [...Array(256).keys()].map(digit => [`192.168.0.${digit}`, `192.168.1.${digit}`]).flat()
+
+/**
+ * Send the heart rate data to the host.
+ * 
+ * @param {InboxItem} file: that has just been received from the smartwatch with heart rate samples
+ * @returns {String} the result of the attempt to send the heart rate data to the host
+ */
+const sendFileToHost = async file => {
+
+    let response;
+
+    try {
+        const buffer = await file.arrayBuffer();
+
+        response = await fetch(`http://${hostIp}:${hostPort}/fitbit-endpoint`, {
+            method: "POST",
+            body: buffer,
+            headers: {"Content-length": buffer.byteLength, "X_FITBIT_FILENAME": file.name}
+        });
+
+        response = await response.text()
+
+    } catch( error ) {
+        console.error(`Error when trying to send heart rate data to host: ${error}`);
+        response = "ERROR"
+    }
+    
+    return response
+}
+
+/**
+ * Send a GET request to the given IP address to check if it corresponds to the host
+ * 
+ * @param {String} ipAddress: the IP address that will be pinged to check if it corresponds to the host IP
+ * @returns {Boolean} if the ping to the IP address has been successful or not
+ */
+const pingIp = ipAddress => new Promise(async (resolve, reject) => {
+
+    try {
+        // Prevent this request to be hanging indefinitely.
+        // If it has not been resolved after 4 seconds, discard this IP address.
+        setTimeout(() => resolve(false), 4000);
+
+        const response = await fetch(`http://${ipAddress}:${hostPort}/fitbit-ping`);
+
+        // Only accept the ipAddress as the host IP address if the response is successful
+        // and the message received is the expected one "FITBIT_HOST"
+        if (response.ok && await response.text() == "FITBIT_HOST") {
+            hostIp = ipAddress;
+            resolve(true)
+        }
+
+        // Otherwise this ipAddress is not valid
+        resolve(false);
+
+    // If an error occurs due to the fetch function, also discard this IP address
+    } catch (error) {
+        resolve(false)
+    }
+
+});
+
+// Periodically look for the host IP address or check if the host IP address is still ok
+setInterval(async () => {
+
+    // Prevent this function to run multiple times concurrently
+    if ( lookingForHostIp ) return;
+
+    lookingForHostIp = true;
+
+    // If a host IP address has already been defined, check if it is still up
+    if ( hostIp && await pingIp(hostIp) ) {
+
+        await outbox.enqueue("host_ip", hostIp.toArrayBuffer());
+        lookingForHostIp = false;
+
+        return;
+    }
+
+    // If the above is not the case, a new host IP address must be found or let the smartwatch
+    // know that there is no host IP address any more
+    hostIp = "";
+    
+    // Look for the host IP among all the defined IP addresses
+    await Promise.all(LOCAL_NETWORK_IP_ADDRESSES.map(ipAddress => pingIp(ipAddress)));
+    await outbox.enqueue("host_ip", hostIp.toArrayBuffer());
+
+    lookingForHostIp = false;
+
+}, 10000); // look for host IP addresses every 10 seconds
+
+/**
+ * Callback for the event of file received.
+ * Read each received file form the smartwatch and act accordingly.
+ */
+const processIncomingFiles = async () => {
+    let file;
+
+    while (( file = await inbox.pop() )) {
+
+        // If file has been successfully sent to the host, tell the smartwatch to delete it
+        if (await sendFileToHost(file) == "OK") {
+            outbox.enqueue("delete_file", file.name.toArrayBuffer());
+        }
+
+    }
+}
+
+// Set the callback for the event of file received from smartwatch
+inbox.addEventListener("newfile", processIncomingFiles);
+processIncomingFiles(); // execute it already in case there is any file in queue
---
+++ server.py
@@ -0,0 +1,103 @@
+import re
+from collections import namedtuple
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+HttpResponse = namedtuple("HttpResponse", ["code", "message"])
+
+class HttpRequestHandler(BaseHTTPRequestHandler):
+    """
+    Process incoming requests.
+
+    Args:
+        BaseHTTPRequestHandler (class): Most basic handler class whose functions must be extended
+        to properly request the different types of requests.
+    """
+    
+    def send_http_response(self, http_response):
+        """
+        Send HTTP response with the given code and message.
+        
+        Args:
+            http_response (HttpResponse): Instance of the HttpResponse namedtuple class initialized with
+            a proper code and message.
+        """
+        
+        # Set response code
+        self.send_response(http_response.code)
+        self.end_headers()
+        
+        # Send response message
+        self.wfile.write(http_response.message.encode("utf-8"))
+    
+    
+    def do_GET(self):
+        """
+        Override do_GET method of the BaseHTTPRequestHandler class to process GET requests.
+        
+        Answer the PING from the Fitbit device so that it can send to this server address the heart rate data.
+        """
+        
+        # Ensure correct path of the request
+        if re.search(r'fitbit\-ping\/?$', self.path) is None:
+            return
+
+        self.send_http_response(HttpResponse(200, "FITBIT_HOST"))
+        
+        
+    def do_POST(self):
+        """
+        Override do_POST method of the BaseHTTPRequestHandler class to process POST requests.
+        
+        Store the heart rate data sent by the Fitbit device when it is sent properly:
+        - Correct path (/fitbit-endpoint/)
+        - Correct headers:
+        - - Content-length: number of bytes, namely heart rate samples, present in the file
+        - - X_FITBIT_FILENAME: name of the file that will store the heart rate samples
+        """
+        
+        # Ensure correct path of the request
+        if re.search(r'fitbit\-endpoint\/?$', self.path) is None:
+            return
+        
+        # Get from the request the number of heart rate samples and the filename to store them  
+        try:
+            nbytes = int(self.headers["Content-length"])
+            filename = self.headers["X_FITBIT_FILENAME"]
+            
+        # Some of header is missing, request ill formed
+        except KeyError as e:
+            self.send_http_response(HttpResponse(400, "Bad request"))
+            return
+        
+        # Store the heart rate samples in the given filename, each byte is a heart rate sample
+        try:
+            heart_rate_samples = self.rfile.read(nbytes)
+            
+            with open(filename, "w") as fitbit_file:
+                fitbit_file.write(",".join([str(sample) for sample in heart_rate_samples]))
+            
+            self.send_http_response(HttpResponse(200, "OK"))
+            
+        # Unexpected error when storing heart rate samples:
+        except Exception as e:
+            print("Unexpected error when storing heart rate samples:", e)
+            self.send_http_response(HttpResponse(500, "Server error"))
+
+
+def start_server(server_class=HTTPServer, handler_class=HttpRequestHandler):
+    """
+    Start a HTTP server to communicate with the Fitbit device.
+
+    Args:
+        server_class (class, optional): The type of server to initialize. Defaults to HTTPServer.
+        handler_class (class, optional): Class with the functions that will handle the requests. Defaults to HttpRequestHandler.
+        
+    Check https://docs.python.org/es/3/library/http.server.html for more information on the type of servers and request handlers available.
+    """
+    
+    server_address = ('', 12345) # empty IP to listen on all network interfaces
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+    
+if __name__ == "__main__":
+    start_server()
